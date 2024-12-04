832cc00d213b6a46e9cf591308ed62ce27035908
Donato Carola <don94@live.it>
2024-12-04 15:41:49 +0100

    updated carousel

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -5,6 +5,7 @@ const url = "/data.json";
 
 const Carousel = () => {
   const [city, setCity] = useState([]);
+  const [active, setActive] = useState(0);
 
   const getData = async () => {
     const response = await axios.get(url);
@@ -12,52 +13,54 @@ const Carousel = () => {
     setCity(response.data);
   };
 
+  const nextSlide = () => {
+    setActive((prevValue) => (prevValue + 1) % city.length);
+  };
+
+  const prevSlide = () => {
+    setActive((prevValue) => (prevValue - 1 + city.length) % city.length);
+  };
+
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <div className=" container mx-auto">
-      <div className="carousel">
-        {city.length > 0 && (
-          <>
-            {city.map((el, index) => {
-              const { id, img, name, description } = el;
-              return (
-                <div
-                  key={id}
-                  id={`id${index + 1}`}
-                  className="carousel-item relative w-full"
-                >
+    <>
+     <div className="container mx-auto">
+      {city.length > 0 && (
+        <div className="card-container relative">
+          {city.map((el, index) => {
+            const { id, img, name, description } = el;
+            return (
+              <div
+                key={id}
+                className={`card card-compact bg-base-100 shadow-xl absolute top-0 left-0 w-full transition-opacity duration-500 ${
+                  index === active ? "opacity-100 z-10" : "opacity-0 z-0"
+                }`}
+              >
+                <figure>
                   <img src={img} alt={name} className="w-full" />
-
-                  <div className="absolute bottom-5 left-5 text-white">
-                    <p className="text-xl font-bold">{name}</p>
-                    <p>{description}</p>
-                  </div>
-                  <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    {/* Pulsante precedente */}
-                    <a
-                      href={`#id${index === 0 ? city.length : index}`}
-                      className="btn btn-circle"
-                    >
-                      ❮
-                    </a>
-                    {/* Pulsante successivo */}
-                    <a
-                      href={`#id${index === city.length - 1 ? 1 : index + 2}`}
-                      className="btn btn-circle"
-                    >
-                      ❯
-                    </a>
-                  </div>
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{name}</h2>
+                  <p>{description}</p>
                 </div>
-              );
-            })}
-          </>
-        )}
-      </div>
+              </div>
+            );
+          })}
+          <div className="flex justify-between absolute top-52  translate-y-1/2   w-full px-2 z-30">
+            <button className="btn" onClick={prevSlide}>
+              Prev
+            </button>
+            <button className="btn" onClick={nextSlide}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
+    </>
   );
 };
 
